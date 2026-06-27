346b5d9fa08a9459e60d6bba295d94b0c3e26af2
z47cg46ky2-beep <z47cg46ky2@privaterelay.appleid.com>
2026-06-27 12:20:23 +0300

    Add type annotations

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 class Person:
     people = {}
 
-    def __init__(self, name, age):
+    def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
         Person.people[name] = self
