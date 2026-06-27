11fd9ccbde114b44bef7d57bcb085f524fbcbeca
z47cg46ky2-beep <z47cg46ky2@privaterelay.appleid.com>
2026-06-27 12:08:10 +0300

    Implement Person class

--- app/main.py
+++ app/main.py
@@ -1,8 +1,23 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        Person.people[name] = self
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    result = []
+
+    for data in people:
+        result.append(Person(data["name"], data["age"]))
+
+    for data, person in zip(people, result):
+        if data.get("wife"):
+            person.wife = Person.people[data["wife"]]
+
+        if data.get("husband"):
+            person.husband = Person.people[data["husband"]]
+
+    return result
