71f14b264c46e18b9a5d55d0f402f8b4275c6425
z47cg46ky2-beep <z47cg46ky2@privaterelay.appleid.com>
2026-06-27 12:15:10 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,7 +1,6 @@
 class Person:
-    people = {}
-
-    def __init__(self, name, age):
+    people: dict = {}
+    def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
         Person.people[name] = self
